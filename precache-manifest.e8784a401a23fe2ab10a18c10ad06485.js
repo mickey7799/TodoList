5ed19cf8bf25c9eee7a9b69c0bad01a29deb03ca
Mickey <vickeylee.w@gmail.com>
2020-06-27 15:27:17 +1000

    Updates

---
+++ precache-manifest.e8784a401a23fe2ab10a18c10ad06485.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f000876a24331e74930",
+    "url": "/TodoList/static/js/app.b7f5d6ad.chunk.js"
+  },
+  {
+    "revision": "f1ae232303dfaa06df15",
+    "url": "/TodoList/static/js/runtime~app.81bce68d.js"
+  },
+  {
+    "revision": "94b32bbf3d51720c996f",
+    "url": "/TodoList/static/js/2.6f6380e3.chunk.js"
+  },
+  {
+    "revision": "ed6d65e6a2dac2fd2ce36a1b32a69508",
+    "url": "/TodoList/static/media/laptop.ed6d65e6.jpg"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/TodoList/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/TodoList/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/TodoList/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/TodoList/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/TodoList/favicon.ico"
+  },
+  {
+    "revision": "7a9dd561b5e73f5b125551983e97a30a",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "4f94740b70ef80ccbc2cb2bf6c17fc52",
+    "url": "/TodoList/manifest.json"
+  },
+  {
+    "revision": "7338197959a087eb0bfacd2d6cb8e27f",
+    "url": "/TodoList/register-service-worker.js"
+  }
+];
